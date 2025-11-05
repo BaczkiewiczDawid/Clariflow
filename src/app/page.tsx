9530944dc041c43a194ecb222102c8625a840902
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-11-05 12:23:37 +0100

    navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import {Gallery} from "@/components/gallery";
 import {Reviews} from "@/components/reviews";
 import {CallToAction} from "@/components/call-to-action";
 import {Footer} from "@/components/footer";
+import {Navbar} from "@/components/navbar";
 
 export default function Home() {
     return (
-        <div className="flex flex-col mx-auto justify-center gap-12 max-w-[90%] xl:max-w-[80%] xl:gap-y-20 2xl:gap-y-32">
-            <Header/>
-            <Features/>
-            <Gallery/>
-            <Reviews/>
-            <CallToAction/>
-            <Footer/>
+        <div className="flex flex-col justify-center">
+           <Navbar />
+            <main className={"mt-24 flex flex-col mx-auto justify-center gap-12 max-w-[90%] xl:max-w-[80%] xl:gap-y-20 2xl:gap-y-32"}>
+                <Header/>
+                <Features/>
+                <Gallery/>
+                <Reviews/>
+                <CallToAction/>
+                <Footer/>
+            </main>
         </div>
     );
 }
