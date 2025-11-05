2b436ff6a14e3b5af13b3caffde1e793503ab6af
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-11-05 13:07:40 +0100

    add empty pages placeholder

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+    return (
+        <div
+            className="flex flex-col mx-auto justify-center gap-12 max-w-[90%] xl:max-w-[80%] xl:gap-y-20 2xl:gap-y-32">
+            <h1 className="text-4xl font-bold text-center mt-20">Galeria</h1>
+        </div>
+    )
+}
