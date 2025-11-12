30f99fa576e19218f57fc95138493424b511299d
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-11-12 16:54:48 +0100

    fix gallery mobile view

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -2,89 +2,10 @@
 
 import {useEffect, useState} from "react";
 import Image from "next/image";
-import Audi1 from "../../../assets/gallery/audi-rs-etron-1.jpg"
-import Audi2 from "../../../assets/gallery/audi-rs-etron-2.jpg"
-import Audi3 from "../../../assets/gallery/audi-rs-etron-3.png"
-import Audi4 from "../../../assets/gallery/audi-rs-etron-4.jpg"
-import HyundaiBayon1 from "../../../assets/gallery/hyundai-bayon-1.jpg"
-import HyundaiBayon2 from "../../../assets/gallery/hyundai-bayon-2.jpg"
-import HyundaiBayon3 from "../../../assets/gallery/hyundai-bayon-3.jpg"
-import Golf7Gti1 from "../../../assets/gallery/golf-7-gti-1.jpg"
-import Golf7Gti2 from "../../../assets/gallery/golf-7-gti-2.jpg"
-import Golf7Gti3 from "../../../assets/gallery/golf-7-gti-3.jpg"
-import OpelInsignia1 from "../../../assets/gallery/opel-insignia-1.jpg"
-import OpelInsignia2 from "../../../assets/gallery/opel-insignia-2.jpg"
-import OpelInsignia3 from "../../../assets/gallery/opel-insignia-3.jpg"
-import OpelInsignia4 from "../../../assets/gallery/opel-insignia-4.jpg"
-import VwTouareg1 from "../../../assets/gallery/vw-touareg-1.jpg"
-import VwTouareg2 from "../../../assets/gallery/vw-touareg-2.jpg"
-import VwTouareg3 from "../../../assets/gallery/vw-touareg-3.jpg"
-
-const cars = [
-    {
-        name: "Audi RS E-Tron GT",
-        description: "Pełny detailing wnętrza i nadwozia w elektrycznym superaucie Audi RS E-Tron GT. Auto przeszło kompleksowe czyszczenie i odświeżenie każdego detalu – od dokładnego mycia nadwozia z deironizacją i dekontaminacją, po renowację wnętrza z pielęgnacją tapicerki skórzanej. Celem realizacji było przywrócenie fabrycznego blasku i ochrona powierzchni przed codziennymi zabrudzeniami.",
-        services: [
-            "Detailing wnętrza",
-            "Mycie detailingowe",
-            "Deironizacja lakieru",
-            "Czyszczenie skór",
-        ],
-        images: [
-            Audi1, Audi3, Audi2, Audi4
-        ],
-    },
-    {
-        name: "Hyundai Bayon",
-        description:
-            "Kompleksowy detailing Hyundaia Bayon obejmujący pełne czyszczenie wnętrza i nadwozia. Auto przeszło dokładne mycie detailingowe z deironizacją lakieru oraz pranie tapicerki i pielęgnację wnętrza. Efekt – świeżość, głębia koloru i perfekcyjna czystość w każdym zakamarku",
-        services: [
-            "Detailing wnętrza",
-            "Mycie detailingowe",
-            "Deironizacja lakieru",
-            "Pranie tapicerki",
-        ],
-        images: [HyundaiBayon3, HyundaiBayon2, HyundaiBayon1],
-    },
-    {
-        name: "Golf 7 GTI",
-        description:
-            "Golf 7 GTI przeszedł kompleksowy detailing wnętrza i nadwozia. Lakier został dokładnie oczyszczony i poddany pełnej korekcie, a wnętrze starannie wyczyszczono i zabezpieczono tapicerkę skórzaną. Dodatkowo zadbano o impregnację plastików, co zapewnia świeży wygląd i ochronę detali. Efekt – perfekcyjna czystość, głębia koloru i eleganckie wykończenie każdego elementu auta.",
-        services: [
-            "Detailing wnętrza",
-            "Mycie detailingowe",
-            "Pełna korekta lakieru",
-            "Czyszczenie oraz zabezpieczenie tapicerki skórzanej",
-            "Impregnacja plastików wewnętrznych",
-        ],
-        images: [Golf7Gti1, Golf7Gti2, Golf7Gti3],
-    },
-    {
-        name: "Opel Insignia",
-        description:
-            "Opel Insignia przeszedł kompleksowy detailing wnętrza i nadwozia. Auto zostało dokładnie umyte, wnętrze starannie oczyszczone, a tapicerka skórzana wyczyszczona i zabezpieczona. Dodatkowo przeprowadzono mycie silnika i pielęgnację detali, co zapewnia świeży wygląd, głębię koloru i perfekcyjną czystość w każdym zakamarku.",
-        services: [
-            "Detailing wnętrza",
-            "Mycie detailingowe",
-            "Mycie silnika",
-            "Czyszczenie oraz zabezpieczenie tapicerki skórzanej",
-        ],
-        images: [OpelInsignia1, OpelInsignia4, OpelInsignia2, OpelInsignia3],
-    },
-    {
-        name: "Volkswagen Touareg",
-        description:
-            "Opel Insignia przeszedł kompleksowy detailing wnętrza i nadwozia. Auto zostało dokładnie umyte, wnętrze starannie oczyszczone, a tapicerka skórzana wyczyszczona i zabezpieczona. Dodatkowo przeprowadzono mycie silnika i pielęgnację detali, co zapewnia świeży wygląd, głębię koloru i perfekcyjną czystość w każdym zakamarku.",
-        services: [
-            "Detailing wnętrza",
-            "Mycie silnika",
-        ],
-        images: [VwTouareg3, VwTouareg2, VwTouareg1],
-    },
-];
+import {mainGalleryList} from "@/data/mainGalleryList";
 
 export default function GalleryPage() {
-    const [selectedCar, setSelectedCar] = useState<typeof cars[0] | null>(null);
+    const [selectedCar, setSelectedCar] = useState<typeof mainGalleryList[0] | null>(null);
     const [currentImage, setCurrentImage] = useState(0);
 
     useEffect(() => {
@@ -125,7 +46,7 @@ export default function GalleryPage() {
                     Galeria Realizacji
                 </h1>
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {cars.map((car, i) => (
+                    {mainGalleryList.map((car, i) => (
                         <div
                             key={i}
                             className="group relative cursor-pointer overflow-hidden rounded-xl shadow-md hover:shadow-lg transition"
---
+++ src/data/mainGalleryList.ts
@@ -0,0 +1,80 @@
+import Audi1 from "../../assets/gallery/audi-rs-etron-1.jpg"
+import Audi2 from "../../assets/gallery/audi-rs-etron-2.jpg"
+import Audi3 from "../../assets/gallery/audi-rs-etron-3.png"
+import Audi4 from "../../assets/gallery/audi-rs-etron-4.jpg"
+import HyundaiBayon1 from "../../assets/gallery/hyundai-bayon-1.jpg"
+import HyundaiBayon2 from "../../assets/gallery/hyundai-bayon-2.jpg"
+import HyundaiBayon3 from "../../assets/gallery/hyundai-bayon-3.jpg"
+import Golf7Gti1 from "../../assets/gallery/golf-7-gti-1.jpg"
+import Golf7Gti2 from "../../assets/gallery/golf-7-gti-2.jpg"
+import Golf7Gti3 from "../../assets/gallery/golf-7-gti-3.jpg"
+import OpelInsignia1 from "../../assets/gallery/opel-insignia-1.jpg"
+import OpelInsignia2 from "../../assets/gallery/opel-insignia-2.jpg"
+import OpelInsignia3 from "../../assets/gallery/opel-insignia-3.jpg"
+import OpelInsignia4 from "../../assets/gallery/opel-insignia-4.jpg"
+import VwTouareg1 from "../../assets/gallery/vw-touareg-1.jpg"
+import VwTouareg2 from "../../assets/gallery/vw-touareg-2.jpg"
+import VwTouareg3 from "../../assets/gallery/vw-touareg-3.jpg"
+
+export const mainGalleryList = [
+    {
+        name: "Audi RS E-Tron GT",
+        description: "Pełny detailing wnętrza i nadwozia w elektrycznym superaucie Audi RS E-Tron GT. Auto przeszło kompleksowe czyszczenie i odświeżenie każdego detalu – od dokładnego mycia nadwozia z deironizacją i dekontaminacją, po renowację wnętrza z pielęgnacją tapicerki skórzanej. Celem realizacji było przywrócenie fabrycznego blasku i ochrona powierzchni przed codziennymi zabrudzeniami.",
+        services: [
+            "Detailing wnętrza",
+            "Mycie detailingowe",
+            "Deironizacja lakieru",
+            "Czyszczenie skór",
+        ],
+        images: [
+            Audi1, Audi3, Audi2, Audi4
+        ],
+    },
+    {
+        name: "Hyundai Bayon",
+        description:
+            "Kompleksowy detailing Hyundaia Bayon obejmujący pełne czyszczenie wnętrza i nadwozia. Auto przeszło dokładne mycie detailingowe z deironizacją lakieru oraz pranie tapicerki i pielęgnację wnętrza. Efekt – świeżość, głębia koloru i perfekcyjna czystość w każdym zakamarku",
+        services: [
+            "Detailing wnętrza",
+            "Mycie detailingowe",
+            "Deironizacja lakieru",
+            "Pranie tapicerki",
+        ],
+        images: [HyundaiBayon3, HyundaiBayon2, HyundaiBayon1],
+    },
+    {
+        name: "Golf 7 GTI",
+        description:
+            "Golf 7 GTI przeszedł kompleksowy detailing wnętrza i nadwozia. Lakier został dokładnie oczyszczony i poddany pełnej korekcie, a wnętrze starannie wyczyszczono i zabezpieczono tapicerkę skórzaną. Dodatkowo zadbano o impregnację plastików, co zapewnia świeży wygląd i ochronę detali. Efekt – perfekcyjna czystość, głębia koloru i eleganckie wykończenie każdego elementu auta.",
+        services: [
+            "Detailing wnętrza",
+            "Mycie detailingowe",
+            "Pełna korekta lakieru",
+            "Czyszczenie oraz zabezpieczenie tapicerki skórzanej",
+            "Impregnacja plastików wewnętrznych",
+        ],
+        images: [Golf7Gti1, Golf7Gti2, Golf7Gti3],
+    },
+    {
+        name: "Opel Insignia",
+        description:
+            "Opel Insignia przeszedł kompleksowy detailing wnętrza i nadwozia. Auto zostało dokładnie umyte, wnętrze starannie oczyszczone, a tapicerka skórzana wyczyszczona i zabezpieczona. Dodatkowo przeprowadzono mycie silnika i pielęgnację detali, co zapewnia świeży wygląd, głębię koloru i perfekcyjną czystość w każdym zakamarku.",
+        services: [
+            "Detailing wnętrza",
+            "Mycie detailingowe",
+            "Mycie silnika",
+            "Czyszczenie oraz zabezpieczenie tapicerki skórzanej",
+        ],
+        images: [OpelInsignia1, OpelInsignia4, OpelInsignia2, OpelInsignia3],
+    },
+    {
+        name: "Volkswagen Touareg",
+        description:
+            "Opel Insignia przeszedł kompleksowy detailing wnętrza i nadwozia. Auto zostało dokładnie umyte, wnętrze starannie oczyszczone, a tapicerka skórzana wyczyszczona i zabezpieczona. Dodatkowo przeprowadzono mycie silnika i pielęgnację detali, co zapewnia świeży wygląd, głębię koloru i perfekcyjną czystość w każdym zakamarku.",
+        services: [
+            "Detailing wnętrza",
+            "Mycie silnika",
+        ],
+        images: [VwTouareg3, VwTouareg2, VwTouareg1],
+    },
+];
